7f364879bfa113a9ce6d5d1d2cd26cd467689384
Tulio Eduardo Vitoriano do Carmo <137341640+TulioEduardo12@users.noreply.github.com>
2023-08-25 22:54:18 +0000

    calculadora do zero

---
+++ calcTulio.py
@@ -0,0 +1,13 @@
+conta = input("escolha a operação a ser feita, adição(A),subtraçao(S),divisao(D) ou multiplicaçao(M) tudo em minusculo ai amigao ")
+
+a = int(input("digite um numero "))
+b = int(input("digite um outro numero "))
+
+if conta == "a":
+    print (a+b)
+elif conta == "s":
+    print (a-b)
+elif conta == "a":
+    print (a/b)
+elif conta == "m":
+    print (a*b)
