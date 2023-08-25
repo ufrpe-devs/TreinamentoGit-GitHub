c655ffa56f82b6dc2f398f6ace5de8ef4ce49259
João Vinícius <jvinicius1505@gmail.com>
2023-08-25 22:54:02 +0000

    Adicionei as funções de multiplicação e divisão

---
+++ calculadoraJoao.py
@@ -0,0 +1,19 @@
+def soma(a,b):
+    return a + b
+
+def subt(a,b):
+    return a - b
+
+def mult(a,b):
+    return a * b
+
+def div(a,b):
+    return a / b
+
+def ehPar(a):
+    if a % 2 == 0 :
+        return "É par!"
+    else:
+        return "Não, é ímpar"
+
+print(ehPar(23))
